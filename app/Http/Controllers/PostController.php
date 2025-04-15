6b230771f4c6b593a9a105738307cf259707e0cb
Gul Muhammad <gulmuhammad57@yahoo.com>
2025-04-15 12:46:30 +0500

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    public function index()
+    /**
+     * @public function index()
+     */
+    public function index() :Response
     {
         return Inertia::render('Dashboard',[
             'posts' => Post::all(),
